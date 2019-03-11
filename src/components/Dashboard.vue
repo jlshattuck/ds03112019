8fb2bbc472981c02434f8b79fe8b8f7734167f7a
Jim Shattuck <jshattuck@statticket.com>
2019-03-11 11:08:45 -0400

    added ticket list to dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <div v-if="showTickets" style="border:red thin solid">
-        <h2>Tickets</h2>
-
+    <div
+      v-if="showTickets"
+      style="border:red thin solid"
+    >
+      <h2>Tickets</h2>
+      <ul>
+        <li v-for="ticket in tickets">
+          Id: {{ticket.Id}} Comment: {{ticket.Comments}} Created: {{ticket.DateCreated}}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -14,7 +21,12 @@ export default {
   data () {
     return {
       title: "Dashboard",
-      showTickets:true
+      showTickets: true,
+      tickets: [
+        { Id: '1', Comments: 'pc is slow', DateCreated: '03/11/2019 03:20' },
+        { Id: '2', Comments: 'cannot login', DateCreated: '03/11/2019 05:20' },
+        { Id: '3', Comments: 'powerchart crashsed', DateCreated: '03/11/2019 16:20' }
+      ]
     }
   },
 }
